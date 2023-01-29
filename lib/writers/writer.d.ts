38a51709c146514c5aabeba043b0aebf1d2fc9c3
pipe-dev <andres.ospina12@utp.edu.co>
2023-01-29 15:04:09 -0500

    feature: remove dist files

--- lib/writers/writer.d.ts
+++
@@ -1,5 +0,0 @@
-export interface Writer {
-    write(name: string): string | Promise<string>;
-    writeAnyOf(filenames: string[]): string | Promise<string | undefined>;
-    writeFromGit(skeletonUrl: string): void;
-}
